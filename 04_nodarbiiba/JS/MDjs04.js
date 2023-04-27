05c961dd9b9704c384c386eda8de20eab61337ae
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-04-27 22:18:16 +0300

    20230427_05_04

---
+++ 04_nodarbiiba/JS/MDjs04.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Date";
+    ;
+}
+
+function myFunction() {
+    document.getElementById("demo1").style.fontSize="35px";   
+}
