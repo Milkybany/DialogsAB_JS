695969f948fc212f75acce132b87ce56d71504a0
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-04-29 20:51:24 +0300

    20230429_06_11

--- 06_nodarbiiba/js06/switch.js
+++ 06_nodarbiiba/js06/switch.js
@@ -1,4 +1,6 @@
 let day;
+let dey_= new Date().getDate();
+
 switch (new Date().getDay()) {
   case 0:
     day = "Sunday";
@@ -21,10 +23,33 @@ switch (new Date().getDay()) {
   case  6:
     day = "Saturday";
     console.log(day);
-   // day_ = 7;
-    //break;
+   //day_ = 7;
+    break;
  case  7:
     day = "Saturdayyyyyy";    
     console.log(day);
 }
 document.getElementById("demo_switch").innerHTML = "Today is " + day;
+
+
+switch (new Date().getDay()) {
+    
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+   default:
+    text=("Loking forward to the Working day");
+  }
+  document.getElementById("demo_switch").innerHTML =  document.getElementById("demo_switch").innerHTML + text;
