9573e571f3f4ed233386bde671059d3d52422816
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-04-29 18:34:04 +0300

    20230429_06_03

--- 06_nodarbiiba/js06/if.js
+++ 06_nodarbiiba/js06/if.js
@@ -1,3 +1,9 @@
-if (new Date().getHours() < 18) {
+if (new Date().getHours() < 20) {
     document.getElementById("demo").innerHTML = "Good day!";
-  }
+    document.getElementById("demo").innerHTML =
+    document.getElementById("demo").innerHTML + " Good day one more time!";}
+
+
+    if (new Date().getHours() <18 ) console.log("Good day!");
+
+    console.log("Good day one more time!");
