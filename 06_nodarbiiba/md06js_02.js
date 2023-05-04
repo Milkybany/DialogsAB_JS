f46d5a3e2625fbdad3b46bd8dcc933361524ab4d
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-04 17:26:42 +0300

    20230504_06_02

---
+++ 06_nodarbiiba/md06js_02.js
@@ -0,0 +1,13 @@
+let table = document.querySelector('#table');
+
+for (let i = 0; i < 4; i++) {
+	let tr = document.createElement('tr');
+	
+	for (let i = 0; i < 31; i++) {
+		let td = document.createElement('td');
+		tr.appendChild(td);
+	}
+	
+	table.appendChild(tr);
+}
+
