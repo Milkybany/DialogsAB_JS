cecaddc724196a3a24caba63805bda54b9c1dbd8
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-10 13:21:05 +0300

    20230510_06_7

--- 06_nodarbiiba/Num.Sist.V2.js
+++ 06_nodarbiiba/Num.Sist.V2.js
@@ -1,5 +1,19 @@
+let arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+
 let table = document.querySelector('#table');
-let k = 1;
+
+for (let subArr of arr) {
+	let tr = document.createElement('tr');
+	
+	for (let elem of subArr) {
+		let td = document.createElement('td');
+		td.textContent = elem;
+		tr.appendChild(td);
+	}
+	
+	table.appendChild(tr);
+}
+/*let k = 1;
 for (let i = 0; i < 20; i++) {
 	let tr = document.createElement('tr');
 	
@@ -12,3 +26,5 @@ for (let i = 0; i < 20; i++) {
 	
 	table.appendChild(tr);
 }
+
+'Binary', 'Octal', 'Decimal', 'Hex'*/
