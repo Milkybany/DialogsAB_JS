d2f6c1c7ff9b51858b842342909ee0c985ea07af
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-10 13:35:46 +0300

    20230510_06_8

--- 06_nodarbiiba/Num.Sist.V2.js
+++ 06_nodarbiiba/Num.Sist.V2.js
@@ -1,4 +1,10 @@
-let arr = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+let arr = [[Binary, Octal, Decimal, Hex], 
+[0, 0, 0, 0],
+ [1, 1, 1, 1], 
+ [10, 2, 2, 2],
+  [11, 3, 3, 3],
+[100, 4, 4, 4],
+];
 
 let table = document.querySelector('#table');
 
@@ -27,4 +33,4 @@ for (let i = 0; i < 20; i++) {
 	table.appendChild(tr);
 }
 
-'Binary', 'Octal', 'Decimal', 'Hex'*/
+*/
