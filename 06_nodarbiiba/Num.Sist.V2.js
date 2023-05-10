c947b5ef9b48c69791c4a52fa6aff998df37b96b
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-10 13:09:23 +0300

    20230510_06_6

--- 06_nodarbiiba/Num.Sist.V2.js
+++ 06_nodarbiiba/Num.Sist.V2.js
@@ -1,11 +1,12 @@
 let table = document.querySelector('#table');
 let k = 1;
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 20; i++) {
 	let tr = document.createElement('tr');
 	
-	for (let i = 0; i < 10; i++) {
+	for (let i = 0; i < 4; i++) {
 		let td = document.createElement('td');
-		td.textContent = 'k++';
+		td.textContent = 'k';
+        k++;
         tr.appendChild(td);
 	}
 	
