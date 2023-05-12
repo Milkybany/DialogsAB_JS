38025525654fbb22e8da1f38676015dfb57e8cd6
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-12 21:46:35 +0300

    20230512_07_04

--- 07_nodarbiiba/JS07/while.js
+++ 07_nodarbiiba/JS07/while.js
@@ -1,14 +1,19 @@
 //console.log("Hello World!");
+
 //skaita no 0-9
+
 /*let text = "";
 let i = 0;
 while (i <= 10) {
   text += "<br>The number is " + i;
   i++;
 }
-document.getElementById("demo").innerHTML = text;
+document.getElementById("demo_while").innerHTML = text;
 */
 
+//katrai nākamajai vērtībai pieskaita +2
+
+/*
 //1. varians
 let text = "";
 let i = 1;
@@ -17,7 +22,7 @@ while (i <= 20) {
   text += "<br>" + i;
   i++;
 }
-document.getElementById("demo").innerHTML = text;
+document.getElementById("demo_while").innerHTML = text;
 
 //2. variants
 let text = "";
@@ -27,16 +32,28 @@ while (i <= 20) {
   text += "<br>" + i;
   i = i + 2;
 }
-document.getElementById("demo2").innerHTML = text;
-//katrai nākamajai vērtībai pieskaita +2
+document.getElementById("demo_while").innerHTML = text;
 
+*/
 
-
-
-
-
-//
+//skaita līdz 20 un ja cipars ir mazāks par 10 priekšā pieliek 0
 /*
+let text = "";
+let i = 1;
+while (i <= 20) {
+    if (i < 10)
+        text += "<br>" + "0" + i;
+    
+    else
+text += "<br>" + i;
+    
+  
+  i++;
+}
+document.getElementById("demo_while").innerHTML = text;
+
+*/
+
 let text = "";
 let i = 1;
 while (i <= 20) {
@@ -48,9 +65,10 @@ while (i <= 20) {
   i++;
 }
 document.getElementById("demo_while").innerHTML = text;
-*/
 
-/*
+
+//LABS VARIANTS TABULAI(Skaita lidz 200 un izlidzīna vusu ar 3 cipariem)
+
 text = "";
 i = 1;
 while (i <= 200) {
@@ -64,4 +82,3 @@ while (i <= 200) {
     i++; // i++; // i = i + 1 // 
   }
 document.getElementById("demo_while").innerHTML = text;
-*/
