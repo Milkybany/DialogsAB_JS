07d9f5fc48569994c48d38b1e140dc58ef0d405e
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-12 22:03:17 +0300

    20230512_07_06

--- 07_nodarbiiba/JS07/functions.js
+++ 07_nodarbiiba/JS07/functions.js
@@ -47,3 +47,68 @@ console.log("Otrā rezultāta vērtība pēc funkcijas darbināšanas: " + funkc
   
   document.getElementById("demo_function_2").innerHTML = text;
   */
+
+
+//dažadi varianti ==>
+
+
+  /*
+  1.v
+  function counter(paragraph_name){
+let text = "";
+let i = 1;
+while (i <= 200) {
+    if (i < 10) {
+  text += "<br>" + 0 + 0 + i + " funkcija"; 
+    } else if (i >= 10 && i < 100) { // var arī optimizēt un atstāt tikai (i < 100)
+        text += "<br>" + 0 + i + " funkcija";
+    } else {
+        text += "<br>" + i + " funkcija";
+    }
+    i++;
+  }
+document.getElementById(paragraph_name).innerHTML = text;
+}
+
+counter("demo_function_1");
+
+counter("demo_function_2");
+*/
+
+
+/*
+2.v
+text = "";
+i = 1;
+while (i <= 200) {
+    if (i < 10) {
+  text += "<br>" + 0 + 0 + i; // text = text + "<br>" + i;
+    } else if (i >= 10 && i < 100) {
+        text += "<br>" + 0 + i;
+    } else {
+        text += "<br>" + i;
+    }
+    i++; // i++; // i = i + 1 // 
+  }
+document.getElementById("demo_function_2").innerHTML = text;
+*/
+
+/*
+3.v
+function counter(paragraph_name, a, b){
+  let text = "";
+  let i = 1;
+  while (i <= 20) {
+      if (i % a == 0) {
+    text += "<br>The number is divided by a " + i; 
+      } else if (i % b == 0) {
+      text += "<br>The number is divided by b " + i; 
+      }
+      i++; 
+    }
+  document.getElementById(paragraph_name).innerHTML = text;
+}
+  counter("demo_function_1",2,0);
+  
+  counter("demo_function_2",1,3);
+  */
