0a887c4e765fa8759d611e74029c34a27d354a4f
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-13 19:32:04 +0300

    20230513_08_02

--- 08_nodarbiiba/JS/JS08/js08.js
+++ 08_nodarbiiba/JS/JS08/js08.js
@@ -1,1 +1,18 @@
 //p
+//funkcijas 2 varianti
+/*
+1.
+const factorial = function fac(n) {
+    return n < 2 ? 1 : n * fac(n - 1);
+  };
+  
+  console.log(factorial(3)); // 6
+ 2. 
+  const factorial = function fac(n) {
+    return if(n < 2) return 1;
+    else(n * fac(n - 1));
+  };
+  
+  console.log(factorial(3)); // 6
+  */
+
