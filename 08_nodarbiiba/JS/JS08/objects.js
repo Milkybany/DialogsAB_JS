d934921d10b3bb82a1924332afb96f9be34ed0a7
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-13 21:07:53 +0300

    20230513_08_05

--- 08_nodarbiiba/JS/JS08/objects.js
+++ 08_nodarbiiba/JS/JS08/objects.js
@@ -30,3 +30,18 @@ const person = {
     return this.firstName + " " + this.lastName;
   }
 };
+
+var discrete_variable_new = discrete_variable;
+const array_example_new = array_example;
+
+const car_1 = car;
+car_1.tipe ='Siat';
+console.log(car_1.tipe);
+console.log(car.tipe);
+
+consol person_1 = new Object();
+
+for (let i = 0; i < array_example.length; i++) consol.log(array_example[i]);
+
+for (array_example of array_example) console.log(array_example);
+
