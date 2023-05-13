790f6fa2c110c0c623bd120750c105127d75524e
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-13 19:55:00 +0300

    20230513_08_04

--- 08_nodarbiiba/JS/JS08/objects.js
+++ 08_nodarbiiba/JS/JS08/objects.js
@@ -21,3 +21,12 @@ const array_example = ['a','b'];
 const function_example = function name_for_internal_use(x){return x*x;};
 
 const car = {type:"Fiat", model:"500", color:"white"};
+
+const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
