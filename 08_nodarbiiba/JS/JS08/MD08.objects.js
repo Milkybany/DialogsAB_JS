afbb9cd308462845ad7ea3faca6a450b070970dc
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-17 21:47:48 +0300

    20230517_08_02

---
+++ 08_nodarbiiba/JS/JS08/MD08.objects.js
@@ -0,0 +1,105 @@
+//demo 1-6 - īpašības
+//Properties
+const person = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 50,
+    eyecolor: "blue"
+  };
+  
+  document.getElementById("demo").innerHTML = person["firstname"] + " is " + person["age"] + " years old.";
+
+  //JavaScript for...in Loop
+  const person = {
+    fname:"John",
+    lname:"Doe",
+    age:25
+  }; 
+  
+  let txt = "";
+  for (let x in person) {
+    txt += person[x] + " ";
+  }
+  
+  document.getElementById("demo2").innerHTML = txt;
+
+//Jaunu rekvizītu pievienošana
+const person = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 50,
+    eyecolor: "blue"
+  };
+  
+  person.nationality = "English";
+  document.getElementById("demo3").innerHTML =
+  person.firstname + " is " + person.nationality + ".";
+
+//objekta dzešana
+const person = {
+    firstname: "John",
+    lastname: "Doe",
+    age: 50,
+    eyecolor: "blue"
+  };
+  
+  delete person.age;
+ // delete person["age"]
+  document.getElementById("demo4").innerHTML =
+  person.firstname + " is " + person.age + " years old.";
+
+//
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: {
+    car1: "Ford",
+    car2: "BMW",
+    car3: "Fiat"
+    }
+  }
+  document.getElementById("demo5").innerHTML = myObj.cars.car2;  
+  // 2. myObj.cars['car2'];  
+  // 3. myObj["cars"]["car2"];
+  /* 4. let p1 = "cars";
+let p2 = "car2";
+document.getElementById("demo5").innerHTML = myObj[p1][p2]; */
+
+//masīviem masīvos,
+let x = "";
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: [
+    {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+    {name:"BMW", models:["320", "X3", "X5"]},
+    {name:"Fiat", models:["500", "Panda"]}
+  ]
+}
+
+for (let i in myObj.cars) {
+  x += "<h2>" + myObj.cars[i].name + "</h2>";
+  for (let j in myObj.cars[i].models) {
+    x += myObj.cars[i].models[j] + "<br>";
+  }
+}
+
+document.getElementById("demo6").innerHTML = x;
+
+//metodes
+//this
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  
+  document.getElementById("demo7").innerHTML = person.fullName();
+
+  
+
+
+
