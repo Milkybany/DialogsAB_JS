07f7b889bf9cf78402037a551ed00dd6c815ee02
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-18 18:04:09 +0300

    20230518_08_02

--- 08_nodarbiiba/JS/JS08/MD08.objects.js
+++ 08_nodarbiiba/JS/JS08/MD08.objects.js
@@ -99,7 +99,24 @@ const person = {
   
   document.getElementById("demo7").innerHTML = person.fullName();
 
+  //Object.values()
+  const myArray = Object.values(person);
+document.getElementById("demo").innerHTML = myArray;
+
+
+
+  //object constructor
+
+  function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
   
-
-
+  const myFather = new Person("John", "Doe", 50, "blue");
+  const myMother = new Person("Sally", "Rally", 48, "green");
+  
+  document.getElementById("demo").innerHTML =
+  "My father is " + myFather.age + ". My mother is " + myMother.age; 
 
