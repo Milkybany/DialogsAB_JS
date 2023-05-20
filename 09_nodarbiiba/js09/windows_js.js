c585d1e84a62568c9986867d4faf04fe14e3622c
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-20 18:57:04 +0300

    20230520_09_03

--- 09_nodarbiiba/js09/windows_js.js
+++ 09_nodarbiiba/js09/windows_js.js
@@ -99,7 +99,15 @@ let intervalID;
  
 function toggleColor() {
   let e = document.getElementById('flashtext');
+  console.log(e);
+  console.log(e.style);
+  console.log(e.style.color);
+  console.log(e.style.color == "red");
+  console.log(e.style.color == 'red' ? 'blue' : 'red');
+
   e.style.color = e.style.color == 'red' ? 'blue' : 'red';
+  console.log(e.style.color);
+  console.log("\n");
 }
 
 function stop() {
