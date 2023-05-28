5a5556a075646c83c577b798370db36fc0af094d
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-28 19:39:58 +0300

    20230528_11_03

--- 11_nodarbiiba/js/cookies_js11.js
+++ 11_nodarbiiba/js/cookies_js11.js
@@ -1,6 +1,16 @@
 //max cookies size 4096 bite
-
-
+/*
+1. Cookie nolasīšana
+1.1. Cookie filtrēšana (atlasīšana) (domain + path; hostname:port\path1\path2 būs redzams iekš hostname:port\path1\path2 un hostname:port\path1\path2\path3 utt., bet nebūs redzams iekš hostname:port\path1)
+2. Cookie uzdošana (unikāls name iekš domain + path)
+3. Cookie mainīšana
+4. Cookie "dzīves laika" uzdošana (ja laiks nav uzdots - Session)
+5. Cookie izmantošana pēc pārlūka aizvēršanas, atvēršana
+-------
+6. Cookie kodēšana
+7. Cookie dzēšana
+*/
+/*
 var str = document.cookie;
 console.log(" Cookies vertība:" + str);
 
@@ -21,5 +31,25 @@ function display() {
     var expireTime = time + 1000*15;
     now.setTime(expireTime);
     document.cookie = 'cookie=ok;expires='+now.toUTCString()+';path=/';
+    console.log(document.cookie);
+  }
+  */
+
+  var str = document.cookie;
+  console.log("cookie vērtība: " + str);
+  
+  display("username_1", "admin_1", 30);
+  display("username_2", "admin_2");
+
+  function display(cookie_name, cookie_value, cookie_expire_time_sec) {
+    var now = new Date();
+    var time = now.getTime();
+    if (cookie_expire_time_sec != undefined) {
+      var expireTime = time + 1000 * cookie_expire_time_sec;
+      now.setTime(expireTime);
+      document.cookie = cookie_name + "=" + cookie_value + ";expires=" + now.toUTCString() + ";path=/";
+    } else {
+      document.cookie = cookie_name + "=" + cookie_value + ";path=/";
+    }
     console.log(document.cookie);
   }
