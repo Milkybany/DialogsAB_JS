9a682ba30372a10301629d995187ea3188f3a3e0
Milkybany <130856096+Milkybany@users.noreply.github.com>
2023-05-28 19:12:32 +0300

    20230528_11_02

--- 11_nodarbiiba/js/cookies_js11.js
+++ 11_nodarbiiba/js/cookies_js11.js
@@ -1,7 +1,25 @@
 //max cookies size 4096 bite
-var str = document.cookie;
-console.log(" Cookies vertība" + str);
 
-document.cookie = "username=admin";
+
+var str = document.cookie;
+console.log(" Cookies vertība:" + str);
+
+document.cookie = "username=admin_1";
 str = document.cookie;
-console.log("Cookies vertība" + str);
+console.log("Cookies vertība:" + str);
+
+document.cookie = "username=admin_2";
+str = document.cookie;
+console.log("Cookies vertība:" + str);
+
+
+// https://stackoverflow.com/questions/13154552/how-can-i-set-a-cookie-with-expire-time
+display();
+function display() { 
+    var now = new Date();
+    var time = now.getTime();
+    var expireTime = time + 1000*15;
+    now.setTime(expireTime);
+    document.cookie = 'cookie=ok;expires='+now.toUTCString()+';path=/';
+    console.log(document.cookie);
+  }
